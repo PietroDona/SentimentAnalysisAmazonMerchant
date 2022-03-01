443d9ff7ab6ec5df7149b87da69233f538e5de30
PietroDona <dona.pietro@gmail.com>
2022-03-01 13:41:09 +0100

    cleanup clustering

---
+++ SentimentAnalysis/aspect_data_structure.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Aspect:
+    aspect: str
+    rule: int = field(compare=False)
+    sentiment: int = field(init=False, default=None, compare=False)
+
+    def __repr__(self):
+        return f"<Aspect - {self.aspect} | sentiment compound - {self.sentiment} |Rule {self.rule}>"
+
+    def __str__(self):
+        return self.__repr__()
--- SentimentAnalysis/aspect_estraction.py
+++ SentimentAnalysis/aspect_estraction.py
@@ -1,261 +1,177 @@
-from dataclasses import dataclass
+from SentimentAnalysis.aspect_data_structure import Aspect
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import spacy
+
+import pandas as pd
 
 
-@dataclass
-class Aspect:
-    aspect: str
-    modifier: str
-    rule: int
+nlp = spacy.load('en_core_web_md', exclude="ner")
+senticnet = pd.read_csv("SentimentAnalysis/senticnet.csv")
+sia = SentimentIntensityAnalyzer()
 
-    def __post_init__(self):
-        self.aspect = self.aspect.lower()
-        self.modifier = self.modifier.lower()
 
-    def __repr__(self):
-        return f"<Aspect - {self.aspect} | Modifier - {self.modifier} | Rule {self.rule}>"
-
-    def __str__(self):
-        return self.__repr__()
+def senticnet_search(query):
+    '''Search if {query} list of word is present in the senticnet list'''
+    processed_query = ("_".join(query)).lower()
+    return not senticnet[senticnet["Aspect"] == processed_query].empty
 
 
 def rule1(doc):
     '''
-        Rule 1 of Aspect extraction (M = Modifier , A = Aspect)
-        # Adjectival modifier - https://universaldependencies.org/docs/en/dep/amod.html
-         - M is a amod but not a STOPWORD
-         - A is its head.
-
-        EXAMPLE
-        "These are not very amazing headphones"
-        ('very not amazing', 'headphones')
+    Aspect extraction following Subject Noun Rule 1
+    There is a subject of t that has any adverbial or adjective modifier.
+    T is the aspect
     '''
     for token in doc:
-        M = None
-        A = None
-        if token.dep_ == "amod" and not token.is_stop:
-            M = token.text
-            A = token.head.text
-
-            # We add all possible adverbial modifier that
-            # change the strength of the modifier
-            # (advmod - https://universaldependencies.org/docs/en/dep/advmod.html)
-            # and negations of the amod
-            for child_m in token.children:
-                if(child_m.dep_ == "neg"):
-                    M = f"not {M}"
-
-                if(child_m.dep_ == "advmod"):
-                    M = f"{child_m.text} {M}"
-        if M and A:
-            return Aspect(modifier=M, aspect=A, rule=1)
-    return None
+        if token.dep_ in ["nsubj", "nsubjpass"]:
+            for child in token.head.children:
+                if child.dep_ in ["amod", "advmod"] and not token.head.is_stop:
+                    if senticnet_search([token.head.lemma_]):
+                        return Aspect(aspect=token.head.lemma_, rule=1)
 
 
 def rule2(doc):
     '''
-        Rule 2 of Aspect extraction (M = Modifier , A = Aspect)
-        Direct Object - http://universaldependencies.org/docs/en/dep/dobj.html
-         - Token (usually VERB representing an action)
-         - M is a child with relationship of dobj (accusative object)
-         - A is a child with relationship of nsubj (nominal subject)
-        Assumption - A verb will have only one NSUBJ and DOBJ
-
-
+    Aspect extraction following Subject Noun Rule 2
+    Sentence without auxiliary verbs and t with adjective, adverbial or adverbial modifier clause with another token -> h and t are aspects
+    or with direct object relation with a NOUN n, n is aspect if in SentiNet
+    or with direct object relation with a NOUN n and not in SentiNet derive list of connected nouns and that is aspect
+    or open clausal complement with another token
     '''
     for token in doc:
-        M = None
-        A = None
-        neg_prefix = None
-        for child in token.children:
-            if child.dep_ == "nsubj" and not child.is_stop:
-                A = child.text
+        if token.dep_ in ["nsubj", "nsubjpass"]:
+            # check if an AUX is present
+            aux_presence = [t for t in doc if t.pos_ == "AUX"]
+            for child in token.head.children:
+                if child.dep_ in ["amod", "advmod", "advcl"] and not aux_presence and not token.is_stop:
+                    return Aspect(aspect=token.lemma_, rule=2)
+                if child.dep_ == "dobj" and child.pos_ == "NOUN" and not aux_presence and not child.is_stop:
+                    if senticnet_search([child.lemma_]):
+                        return Aspect(aspect=child.lemma_, rule=2)
+                    else:
+                        tmp = " ".join([child.lemma_]+[cococ.lemma_ for coc in child.children if coc.pos_ ==
+                                                       "ADP" for cococ in coc.children if cococ.pos_ == "NOUN"])
+                        return Aspect(aspect=tmp,  rule=2)
 
-            if child.dep_ == "dobj" and not child.is_stop:
-                M = child.text
-                for child_of_child in child.children:
-                    if child_of_child.text.lower() in ["no", "not"]:
-                        M = f"{child_of_child.text} {M}"
-
-            # If amongs the childs we find a negation we add it to the modifier
-            # This will not work in case of double negations/corner cases
-            if child.dep_ == "neg":
-                neg_prefix = child.text
-
-        if (neg_prefix and M):
-            M = f"{neg_prefix} {M}"
-        if M and A:
-            return Aspect(modifier=M, aspect=A, rule=2)
-    return None
+                if child.dep_ == "xcomp" and not child.is_stop:
+                    # if [child,coc] is in SenticNet
+                    tmp = [[child.lemma_, coc.lemma_]
+                           for coc in child.children]
+                    for coc in child.children:
+                        if senticnet_search([child.lemma_, coc.lemma_]) or senticnet_search([coc.lemma_, child.lemma_]):
+                            return Aspect(aspect=" ".join([child.lemma_, coc.lemma_]), rule=2)
+                    else:
+                        tmp = [
+                            cococ.lemma_ for coc in child.children for cococ in coc.children if cococ.pos_ == "NOUN"]
+                        if tmp:
+                            return Aspect(aspect=" ".join(tmp), rule=2)
 
 
 def rule3(doc):
     '''
-        Rule 3 of Aspect extraction (M = Modifier , A = Aspect)
-        Adjectival Complement
-         - Token (usually VERB representing an action)
-         - M is a child with relationship of acomp (adjectival complement)
-         - A is a child with relationship of nsubj (nominal subject)
-        EXAMPLE
-        "These headphones are not amazing"
-        ('amazing', 'headphones')
+    Subject Noun Rule
+    Sentence with auxiliary verb (copula) and token as complement -> token is aspect
     '''
     for token in doc:
-        M = None
-        A = None
-        neg_prefix = None
-        for child in token.children:
-            if child.dep_ == "nsubj" and not child.is_stop:
-                A = child.text
-            # Work around imperative sentences
-            if child.head.dep_ == "ROOT" and not A:
-                A = child.head.text
-            if(child.dep_ == "acomp" and not child.is_stop):
-                M = child.text
-
-            # We need to keep the auxiliaries and negations
-            # We ignore double negations
-            if(child.dep_ == "aux" and child.tag_ == "MD") or (child.dep_ == "neg"):
-                neg_prefix = "not"
-
-        if (neg_prefix and M):
-            M = f"{neg_prefix} {M}"
-
-        if M and A:
-            return Aspect(modifier=M, aspect=A, rule=3)
-    return None
+        if token.dep_ in ["nsubj", "nsubjpass"]:
+            for child in token.head.children:
+                if child.dep_ in ["acomp"] and token.head.pos_ == "AUX" and not child.is_stop:
+                    # check if child exists in the implicit aspect lexicon
+                    # print(child)
+                    return Aspect(aspect=child.lemma_, rule=3)
 
 
 def rule4(doc):
     '''
-        Rule 4 of Aspect extraction(M=Modifier, A=Aspect)
-        Adverbial modifier to a passive verb
-         - Token
-            - M is a child with relationship of advmod (adverbial modifier)
-            - A is a child with relationship of nsubjpass (passive nominal subject)
-        EXAMPLE
-        "These headphones work incredibly"
-        ('incredibly', 'headphones')
+    Subject Noun Rule
+    Sentence with auxiliary verb (copula) and token as complement and a Noun -> noun is aspect
     '''
     for token in doc:
-        M = None
-        A = None
-        neg_prefix = None
-        for child in token.children:
-            if((child.dep_ == "nsubjpass" or child.dep_ == "nsubj") and not child.is_stop):
-                A = child.text
-
-            if(child.dep_ == "advmod" and not child.is_stop):
-                M = child.lemma_
-                # Add additional modifiers
-                for child_m in child.children:
-                    if(child_m.dep_ == "advmod"):
-                        M = f"{child_m.text} M"
-            # Add negations
-            if child.dep_ == "neg":
-                neg_prefix = "not"
-
-        if (neg_prefix and M):
-            M = f"{neg_prefix} {M}"
-
-        if M and A:
-            return Aspect(modifier=M, aspect=A, rule=4)
-    return None
+        if token.dep_ in ["nsubj", "nsubjpass"]:
+            for child in token.head.children:
+                if child.dep_ in ["acomp"] and token.head.pos_ == "AUX" and token.pos_ == "NOUN" and not token.is_stop:
+                    return Aspect(aspect=token.lemma_, rule=4)
 
 
 def rule5(doc):
     '''
-        Rule 5 of Aspect extraction(M=Modifier, A=Aspect)
-        Complement of a copular verb
-        A copular verb is a special kind of verb used to join an adjective or noun complement to a subject.  
-        Common examples are: be (is, am, are, was, were), appear, seem, look, sound, smell, taste, feel, become and get.
-         - Token
-            - M is a copular verb ?
-            - A is a nsubj
+    Subject Noun Rule
+    Sentence with auxiliary verb (copula) and token as complement and a Noun -> noun is aspect
     '''
     for token in doc:
-        A = None
-        M = None
-        for child in token.children:
-            if(child.dep_ == "nsubj" and not child.is_stop):
-                A = child.text
-            if(child.dep_ == "cop" and not child.is_stop):
-                M = child.text
-        if M and A:
-            return Aspect(modifier=M, aspect=A, rule=5)
-    return None
+        if token.dep_ in ["nsubj", "nsubjpass"]:
+            for child in token.head.children:
+                if child.dep_ in ["acomp"] and token.head.pos_ == "AUX" and not child.is_stop:
+                    # check if  child and coc exists in the implicit aspect lexicon
+                    tmp = " ".join(
+                        [child.lemma_]+[coc.lemma_ for coc in child.children if coc.pos_ == "VERB"])
+                    return Aspect(aspect=tmp, rule=5)
 
 
 def rule6(doc):
     '''
-        Rule 6 of Aspect extraction(M=Modifier, A=Aspect)
-        Interjection
-        - M is an Interjection
-        - A is its nsubj
+    NO Subject Noun Rule
+    Sentence with adjective or adverb h in infinitival or open clausal complement -> if h in IAC lexicon -> h aspect
     '''
     for token in doc:
-        A = None
-        M = None
-        if(token.pos_ == "INTJ" and not token.is_stop):
+        if token.pos_ in ["ADJ", "ADV"]:
             for child in token.children:
-                if(child.dep_ == "nsubj" and not child.is_stop):
-                    A = child.text
-                    M = token.text
-        if M and A:
-            return Aspect(modifier=M, aspect=A, rule=6)
-    return None
+                if child.dep_ in ["ccomp", "xcomp"] and not token.is_stop:
+                    # if token is in IAC lexicon
+                    return Aspect(aspect=token.lemma_, rule=6)
 
 
 def rule7(doc):
     '''
-        Rule 7 of Aspect extraction(M=Modifier, A=Aspect)
-        Interjection
-        - M is an Interjection
-        - A is its nsubj
+    NO Subject Noun Rule
+    h token connected to noun t through preposition -> h+t aspect
     '''
     for token in doc:
-        M = None
-        A = None
-        neg_prefix = None
         for child in token.children:
-            if child.dep_ == "nsubj" and not child.is_stop:
-                A = child.text
-
-            if child.dep_ == "attr" and not child.is_stop:
-                M = child.text
-            # Add negations
-            if child.dep_ == "neg":
-                neg_prefix = "not"
-
-        if (neg_prefix and M):
-            M = f"{neg_prefix} {M}"
-
-        if M and A:
-            return Aspect(modifier=M, aspect=A, rule=7)
-    return None
+            if child.dep_ == "prep":
+                for child_of_child in child.children:
+                    if child_of_child.pos_ == "NOUN" and not token.is_stop:
+                        return Aspect(aspect=f"{token.lemma_} {child_of_child.lemma_}", rule=4)
 
 
 def rule8(doc):
     '''
-        Rule 8 of Aspect extraction(M=Modifier, A=Aspect)
-        Adposition
-        - M is an adjective in the head of the adposition
-        - A is its obj
+    NO Subject Noun Rule
+    h token connected with direct object with t -> t aspect
     '''
     for token in doc:
-        M = None
-        A = None
-        if token.pos_ == "ADP":
-            if token.head.pos_ == "ADJ":
-                M = token.head.text
-            for child in token.children:
-                if child.dep_ == "pobj" and not child.is_stop:
-                    A = child.text
-
-        if M and A:
-            return Aspect(modifier=M, aspect=A, rule=8)
-    return None
+        for child in token.children:
+            if child.dep_ == "dobj" and not child.is_stop:
+                return Aspect(aspect=child.lemma_, rule=8)
 
 
-rules = {"rule1": rule1, "rule2": rule2, "rule3": rule3,
-         "rule4": rule4, "rule5": rule5, "rule6": rule6,
-         "rule7": rule7, "rule8": rule8}
+def extract_aspect_sentence(doc):
+    '''Extract the aspects from a sentence'''
+    SubjectRule = {"rule1": rule1, "rule2": rule2, "rule3": rule3,
+                   "rule4": rule4, "rule5": rule5}
+    NoSubjectRule = {"rule6": rule6, "rule7": rule7, "rule8": rule8}
+    subjects = [token for token in doc if token.dep_ in ["nsubj", "nsubjpass"]]
+    aspects = []
+    if subjects:
+        for name, rule in SubjectRule.items():
+            if a := rule(doc):
+                if a not in aspects:
+                    a.sentiment = sia.polarity_scores(doc.text).get('compound')
+                    aspects.append(a)
+
+    else:
+        for name, rule in NoSubjectRule.items():
+            if a := rule(doc):
+                if a not in aspects:
+                    a.sentiment = sia.polarity_scores(doc.text).get('compound')
+                    aspects.append(a)
+    return aspects
+
+
+def get_aspects(text: str):
+    doc = nlp(text)
+    aspects = []
+    for sent in doc.sents:
+        tmp_aspects = extract_aspect_sentence(sent)
+        aspects += tmp_aspects
+    return aspects
